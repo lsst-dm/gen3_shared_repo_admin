920b7177dc2e649b725c5442cf304b248fc5f411
taranu <dtaranu@astro.princeton.edu>
2021-09-13 13:35:29 -0500

    Add RC2 w_2021_18 and w_2021_22 to converted weeklies

--- python/lsst/gen3_shared_repo_admin/sites/ncsa/hsc.py
+++ python/lsst/gen3_shared_repo_admin/sites/ncsa/hsc.py
@@ -183,6 +183,8 @@ def rerun_operations() -> Iterator[AdminOperation]:
     in the `/repo/main` data repository at NCSA.
     """
     for weekly, ticket in {
+        "w_2021_22": "DM-30424",
+        "w_2021_18": "DM-29946",
         "w_2021_14": "DM-29519",
         "w_2021_10": "DM-29074",
         "w_2021_06": "DM-28654",
